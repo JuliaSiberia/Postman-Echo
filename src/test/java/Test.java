9aead767a75ae83508bac9132d0b7c10d05931c0
Yuliya Kulpina <iyuliyaraspopina@gmail.com>
2022-10-11 17:13:20 +0800

    Initial commit

---
+++ src/test/java/Test.java
@@ -0,0 +1,20 @@
+import static io.restassured.RestAssured.given;
+
+public class Test {
+    @org.junit.jupiter.api.Test
+    void postRequest() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body(/* --> ваша проверка здесь <-- */)
+        ;
+    }
+}
